1ad2c8b735ebe9291e6847232b0239951162d3b5
Seyit Bahadir Cetin <seyitbahadirr@gmail.com>
2022-07-24 16:43:09 +0300

    - Auth operations are done. - CRUD operations of users and posts are done. - Client react-app created

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,6 @@
+const { AuthenticationError, UserInputError } = require('apollo-server');
+const { argsToArgsConfig } = require('graphql/type/definition');
+
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -5,7 +8,7 @@ module.exports = {
     Query: {
     async getPosts(){
         try {
-            const posts = await Post.find();
+            const posts = await Post.find().sort({ createdAt: -1 });
             return posts;
         } catch (err){
             throw new Error(err);
@@ -27,7 +30,10 @@ module.exports = {
     Mutation: {
        async createPost(_, { body }, context){
             const user = checkAuth(context);
-            console.log(user);
+            
+            if(argsToArgsConfig.body.trim() === ''){
+                throw new Error('Post body muyst not be empty');
+            }
 
             const newPost = new Post({
                 body,
@@ -38,6 +44,42 @@ module.exports = {
              const post = await newPost.save();
 
              return post;
-       } 
+       },
+        async deletePost(_, { postId }, context){
+            const user = checkAuth(context);
+
+            try{
+                const post = await Post.findById(postId);
+                if(user.username === post.username){
+                    await post.delete();
+                    return 'Post deleted successfully';
+                } else {
+                    throw new AuthenticationError('Action not allowed');
+                } 
+            } catch(err){
+                throw new Error(err);
+            }
+        },
+        async likePost(_, { postId }, context){
+            const { username } = checkAuth(context);
+
+            const post = await Post.findById(postId);
+            if(post){
+                if(post.likes.find(like => like.username === username)){
+                    //Post already like, unlike
+                    post.likes = post.likes.filter(like => like.username !== usename)
+                    await post.save();
+
+                } else {
+                    //not liked like
+                    post.likes.push({
+                        username,
+                        createdAt: new Date().toISOString()
+                    })
+                }
+                await post.save();
+                return post;
+            } else throw new UserInputError('Post not found!');
+        }
     }
 };
